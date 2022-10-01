fbc6ece520eb6da40d3560a4e17d8d6027830615
Nuria Toledano <ia.andyouare@gmail.com>
2022-10-01 15:38:31 +0100

    Made the Card dynamic to read data from a file

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,22 @@ import './App.css'
 import Header from './components/Header'
 import Card from './components/Card'
 import Footer from './components/Footer'
+import Data from './data'
 
 function App() {
+  const cards = Data.map(travel => {
+    return (
+      <Card
+        key={travel.id}
+        {...travel}
+      />
+    )
+  })
   return (
     <div className="App">
       <Header />
       <section className="cards-list">
-        <Card />
-        <Card />
+        {cards}
       </section>
       <Footer />
     </div>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,19 +1,19 @@
 import pointer from '../assets/pointer.png'
 
-export default function Card() {
+export default function Card(props) {
   return (
     <div className="card">
-      <img className="card--image" src="https://source.unsplash.com/WLxQvbMyfas" alt="Mont Fuji" />
+      <img className="card--image" src={props.imageUrl} alt="Mont Fuji" />
       <div className="card--info">
       <div className="card--location">
         <img className="card--pointer" src={pointer} alt="location pointer" />
-        <h2>JAPAN</h2>
-        <a href="https://goo.gl/maps/pPgXPiKdQFjPgZap6" className="card--link">View on Google Maps</a>
+        <h2>{props.location.toUpperCase()}</h2>
+        <a href={props.googleMapsUrl} className="card--link">View on Google Maps</a>
       </div>
       <div className="card--description">
-        <h1>Mont Fuji</h1>
-        <h3>12 Jan, 2021 - 24 Jan, 2021</h3>
-        <p>Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.</p>
+        <h1>{props.title}</h1>
+        <h3>{props.startDate} - {props.endDate}</h3>
+        <p>{props.description}</p>
       </div>
       </div>
     </div>
